12370d1d3c7b282e1262113b5bb58fde5c50387b
Twittytop <169054211@qq.com>
2019-12-02 23:47:59 +0800

    feat(Array): 数组扁平化和去重

--- src/Array/index.js
+++ src/Array/index.js
@@ -6,5 +6,34 @@
   * 扁平化n维数组
   */
 
- // ES10 flat方法
+ // 1. ES10 flat方法
+ // 2. 
+ export function flatten(arr) {
+   while (arr.some(item => Array.isArray(item))) {
+     arr = [].concat(...arr);
+   }
+   return arr;
+ }
 
+ /**
+  * 去重
+  */
+
+ // 1. ES6 Set 
+ // Array.from(new Set(arr)) 或者 [...new Set(arr)]
+ // 2. 
+ export function distinct(arr) {
+   let result = [],
+   i, 
+   j,
+   len = arr.length;
+   for(i = 0; i < len; i++) {
+     for(j = i + 1; j < len; j++) {
+       if (arr[i] === arr[j]) {
+          j = ++i;
+       }
+     }
+     result.push(arr[i]);
+   }
+   return result;
+ }
